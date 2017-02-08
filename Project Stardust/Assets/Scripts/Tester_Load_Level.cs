4b68e95ae85df1f187ccab3a3c6cb26dcf291037
elliotviechweg <elliotviechweg@googlemail.com>
2017-02-08 15:36:26 +0000

    OnClick Load Scene
    Added button to load new scene

---
+++ Project Stardust/Assets/Scripts/Tester_Load_Level.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Tester_Load_Level : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().Planet_Shader_WIP);
+    }
+}
