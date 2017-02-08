7a32f414f61e14347baab4f44f6e829f729cd98d
elliotviechweg <elliotviechweg@googlemail.com>
2017-02-08 18:40:47 +0000

    Updated Planet Shader
    Added toon shader for planet and added the ability to tap battle button
    to load next scene

--- Project Stardust/Assets/Scripts/Tester_Load_Level.cs
+++ Project Stardust/Assets/Scripts/Tester_Load_Level.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using System.Collections;
 
-public class Tester_Load_Level : MonoBehaviour {
+public class Tester_Load_Level : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {  
+
+    }
 
     // Update is called once per frame
-    void Update () {
-		
-	}
-
-    public void LoadScene()
+    void Update()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().Planet_Shader_WIP);
+
+    }
+
+    public void TESTERBUTTON()
+    {
+        SceneManager.LoadScene("PlanetShaderWIP");
     }
 }
