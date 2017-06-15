f631b6bb4544df2f5169ce205e7dc44315b970ca
Mickwa9 <mickwa@op.pl>
2017-06-15 20:26:18 +0200

    Swipe through galaxy

---
+++ Project Stardust/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class InputHandler : MonoBehaviour
+{
+  public event Action OnSwipeUp;
+  public event Action OnSwipeDown;
+
+  Vector2 _dragStartPosition;
+  Vector2 _dragEndPosition;
+  Camera _mainCam;
+
+  private void Start()
+  {
+    _mainCam = Camera.main;
+  }
+
+  void Update()
+  {
+    if (Input.GetMouseButtonDown(0))
+    {
+   //   Debug.Log("button pressed");
+      _dragStartPosition = _mainCam.ScreenToViewportPoint(Input.mousePosition);
+    }
+
+    if (Input.GetMouseButtonUp(0))
+    {
+  //    Debug.Log("button released");
+      _dragEndPosition = _mainCam.ScreenToViewportPoint(Input.mousePosition);
+      CheckForSwipe();
+    }
+  }
+
+  private void CheckForSwipe()
+  {
+  //  Debug.Log("button released");
+  //  Debug.Log("s: " + _dragStartPosition + " e: " + _dragEndPosition + " diff : ");
+    float xdiff = _dragEndPosition.x - _dragStartPosition.x;
+    float ydiff = _dragEndPosition.y - _dragStartPosition.y;
+
+    // if horizontal swipe is bigger than vertical than it is not a swipe:
+    if (Mathf.Abs(xdiff) > Mathf.Abs(ydiff)) return;
+    if (ydiff > 0.1f)
+    {
+      Debug.Log("Swipe Up()");
+      if (OnSwipeUp != null) OnSwipeUp();
+    }
+    if (ydiff < -0.1f)
+    {
+      Debug.Log("Swipe Down()");
+      if (OnSwipeDown != null) OnSwipeDown();
+    }
+  }
+}
---
+++ Project Stardust/Assets/Scripts/SelectPlanet.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPlanet : MonoBehaviour
+{
+  enum SwipeState
+  {
+    SWIPE_IN_PROGRESS,
+    WAITING_FOR_SWIPE
+  }
+
+  Camera _mainCamera;
+  InputHandler _inputHandler;
+  Vector3 _cameraStartPosition;
+  Vector3 _cameraEndPosition;
+  float _swipeProgress;
+  SwipeState _state;
+
+  [SerializeField]
+  Transform[] _lookAtplanetPositions;
+
+  int _selectedPlanetIndex = 0;
+  public float Speed;
+  [SerializeField]
+  AnimationCurve _cameraSpeed;
+
+  void Start()
+  {
+    _mainCamera = Camera.main;
+    _inputHandler = FindObjectOfType<InputHandler>();
+    _mainCamera.transform.position = _lookAtplanetPositions[_selectedPlanetIndex].position;
+    _state = SwipeState.WAITING_FOR_SWIPE;
+    AddEventListeners();
+  }
+
+
+  private void Update()
+  {
+    if (_state == SwipeState.SWIPE_IN_PROGRESS)
+      UpdateCameraPosition();
+  }
+
+  private void OnDestroy()
+  {
+    RemoveEventListeners();
+  }
+  void AddEventListeners()
+  {
+    _inputHandler.OnSwipeUp += _inputHandler_OnSwipeUp;
+    _inputHandler.OnSwipeDown += _inputHandler_OnSwipeDown;
+  }
+
+  void RemoveEventListeners()
+  {
+    _inputHandler.OnSwipeUp += _inputHandler_OnSwipeUp;
+    _inputHandler.OnSwipeDown += _inputHandler_OnSwipeDown;
+  }
+
+  private void _inputHandler_OnSwipeDown() // Go to next planet
+  {
+    if (_state == SwipeState.SWIPE_IN_PROGRESS) return;
+    if (_selectedPlanetIndex == _lookAtplanetPositions.Length - 1) return;
+    _cameraStartPosition = _lookAtplanetPositions[_selectedPlanetIndex].position;
+    _selectedPlanetIndex++;
+    
+    _cameraEndPosition = _lookAtplanetPositions[_selectedPlanetIndex].position;
+    _swipeProgress = 0;
+    _state = SwipeState.SWIPE_IN_PROGRESS;
+  }
+
+
+  private void _inputHandler_OnSwipeUp() // Go to prev planet
+  {
+    if (_state == SwipeState.SWIPE_IN_PROGRESS) return;
+    if (_selectedPlanetIndex == 0) return;
+    _cameraStartPosition = _lookAtplanetPositions[_selectedPlanetIndex].position;
+    _selectedPlanetIndex--;
+    _cameraEndPosition = _lookAtplanetPositions[_selectedPlanetIndex].position;
+    _swipeProgress = 0;
+    _state = SwipeState.SWIPE_IN_PROGRESS;
+  }
+
+  private void UpdateCameraPosition()
+  {
+    _swipeProgress += Time.deltaTime*Speed;
+    Vector3 newCamPos = Vector3.Lerp(_cameraStartPosition, _cameraEndPosition, _cameraSpeed.Evaluate(_swipeProgress));
+    _mainCamera.transform.position = newCamPos;
+    Debug.Log("progress: " + _swipeProgress+ " start: "+ _cameraStartPosition+" end: "+_cameraEndPosition+ " campos: "+newCamPos);
+    if (_swipeProgress >= 1f) _state = SwipeState.WAITING_FOR_SWIPE;
+  }
+}
