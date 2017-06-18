8e1986f6e69cb901bf06a7ccd10d48e20f99519c
Mickwa9 <mickwa@op.pl>
2017-06-18 12:45:15 +0200

    Adding TextMeshPRO and some planet UI

---
+++ Project Stardust/Assets/StartBattle.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartBattle : MonoBehaviour
+{
+
+  public void OnButtonPressed()
+  {
+    Debug.Log(":)");
+  }
+}
