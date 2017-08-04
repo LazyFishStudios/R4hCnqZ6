27f91fff559b694a17e9fd5640f3d34cfa9be434
Mickwa9 <mickwa@op.pl>
2017-08-04 16:55:52 +0200

    Add arrow from draged unit instead of dragging it

--- Project Stardust/Assets/Scripts/UnitsDragBehaviour.cs
+++ Project Stardust/Assets/Scripts/UnitsDragBehaviour.cs
@@ -6,6 +6,7 @@ using Zenject;
 /// <summary>
 /// 
 /// Logic for draging ALL Unit views on table.
+/// Draw drag arrow also
 /// 
 /// </summary>
 
@@ -13,6 +14,12 @@ namespace StarDust
 {
   public class UnitsDragBehaviour : MonoBehaviour
   {
+    LineRenderer _lineRenderer;
+    SpriteRenderer _arrowSpriteRenderer;
+
+    [SerializeField]
+    Transform ArrowHead;
+
     Camera cam;
     Vector3 offset;
     Vector3 hitPosition;
@@ -26,9 +33,14 @@ namespace StarDust
 
     [Inject]
     GameLogic _gameLogic;
+
     private void Start()
     {
       cam = Camera.main;
+      _lineRenderer = GetComponent<LineRenderer>();
+      _arrowSpriteRenderer = ArrowHead.GetComponent<SpriteRenderer>();
+      _arrowSpriteRenderer.color = _lineRenderer.endColor;
+      _arrowSpriteRenderer.enabled = false;
     }
 
     private void Update()
@@ -51,8 +63,12 @@ namespace StarDust
         if (hitSuccess && Input.GetMouseButtonDown(0))
         {
           Debug.Log("Drag start");
+          _lineRenderer.enabled = true;
+          _arrowSpriteRenderer.enabled = true;
+          
           isDragging = true;
           offset = hitObject.transform.position - rayHit.point;
+          _lineRenderer.SetPosition(0, hitObject.transform.position);
           // Get distance to camera:
           Vector3 h = rayHit.point - cam.transform.position;
           dist = Vector3.Dot(h, cam.transform.forward);
@@ -62,7 +78,11 @@ namespace StarDust
       if (isDragging)
       {
         Vector3 p = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist));
-        hitObject.transform.position = p + offset;
+       // hitObject.transform.position = p + offset;
+        _lineRenderer.SetPosition(1, p);
+        ArrowHead.position = p;
+        ArrowHead.up = _lineRenderer.GetPosition(1) - _lineRenderer.GetPosition(0);
+
 
         // if we are dragging a card, check if anything else will be hit by raycast:
         hits = Physics.RaycastAll(rayFromCamera);
@@ -72,8 +92,8 @@ namespace StarDust
       if (Input.GetMouseButtonUp(0) && isDragging)
       {
         isDragging = false;
- 
-
+        _lineRenderer.enabled = false;
+        _arrowSpriteRenderer.enabled = false;
         /* 2 is only valid state to call gamelogic.UnitAttacked.
          2 hits means that we are dragging UnitView and it was released when cursor was over secong UnitView.
          I assume that there are no other gameobjects on table than UnitViews.
